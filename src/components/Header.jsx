4ccd2941adf767a08e63240fa1fd81f8890d58b0
irakli85 <iberidze38@gmail.com>
2023-01-17 14:29:39 +0400

    drop down menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../assets/homePage/Taati.png'
 import phone from '../assets/homePage/phone.png'
 import whatsapp from '../assets/homePage/whatsapp.png'
@@ -6,30 +6,61 @@ import telegram from '../assets/homePage/telegram.png'
 
 
 function Header() {
+    const [isHover, setIsHover] = useState(false);
+
+   const handleMouseEnter = () => {
+      setIsHover(true);
+   };
+
+   const handleMouseLeave = () => {
+      setIsHover(false);
+   };
+
+   const boxStyle = {     
+      display: isHover ? 'flex' : 'none',
+   };
+
   return (
     <>        
         <header>
-            <div className="header-img">
-                <img src={logo} alt="logo"/>
+            <div className='header'>
+                <div className="header-img">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <nav>
+                    <a onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave} className='nav-dropdown' href="/">ჩვენს შესახებ</a>
+                    <a onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave} className='nav-dropdown' href="/">განრიგი</a>
+                    <a onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave} className='nav-dropdown' href="/">პროგრამა</a>
+                    <a href="/">ფოტო და ვიდეო</a>
+                    <a href="/">ჩვენი გუნდი</a>
+                    <a href="/">კონტაქტი</a>
+                </nav>
+                <div className="header-contact">
+                    <img src={phone} alt="phone"/>
+                    <span>+995 123 456 789</span>
+                </div>
+                <div className="header-social">
+                    <img src={whatsapp} alt="whatsapp"/>
+                    <img src={telegram} alt="telegram"/>
+                </div>
+                <button className='pulse'>ჩაეწერეთ ექსკურსიაზე</button>
             </div>
-            <nav>
-                <a className='nav-dropdown' href="/">ჩვენს შესახებ</a>
-                <a className='nav-dropdown' href="/">განრიგი</a>
-                <a className='nav-dropdown' href="/">პროგრამა</a>
-                <a href="/">ფოტო და ვიდეო</a>
-                <a href="/">ჩვენი გუნდი</a>
-                <a href="/">კონტაქტი</a>
-            </nav>
-            <div className="header-contact">
-                <img src={phone} alt="phone"/>
-                <span>+995 123 456 789</span>
+
+            <div onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave} style={boxStyle} className='header-dropdown-nav'>
+                <a href='/'>ჩვენს შესახებ</a>
+                <a href='/'>დოკუმენტები</a>
+                <a href='/'>მშობლებს</a>
+                <a href='/'>კვება</a>
+                <a href='/'>სიახლეები</a>
+                <a href='/'>გამოხმაურება</a>
             </div>
-            <div className="header-social">
-                <img src={whatsapp} alt="whatsapp"/>
-                <img src={telegram} alt="telegram"/>
-            </div>
-            <button className='pulse'>ჩაეწერეთ ექსკურსიაზე</button>
+
         </header>
+        
     </>
   )
 }
